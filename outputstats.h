e425a9057eeec3c23542467b2d42602b83f0e026
MarkHohertz <33075054+MarkHohertz@users.noreply.github.com>
2022-10-14 12:42:00 -0500

    Update outputstats.h

--- outputstats.h
+++ outputstats.h
@@ -12,11 +12,12 @@ class outputstats {
 		void setLeverage(double l);         
 		double getLeverage();				
 		int getNumAssets();                 //could get this from statistics
-		void calcTreynor();                //need treynor for both preferred portfolio & S&P 500
+		void calcPortBeta();
+		double calcTreynor(double portRet, double riskFree, double beta);                //need treynor for both preferred portfolio & S&P 500
 		double getTreynor();				
-		void calcSharpe();					//need sharpe for both preferred portfolio and S&P 500
-		//calcSTD()?                   need STD for S&P 500 & Russell 2000
-		//Calc10Yrs()                 calculate value of preferred portfolio and its largest 3 holdings in 10 yrs.
+		double calcSharpe(double portRet, double riskFree, double stDev);					//need a way to calc sharpe for s&p 500
+		double calcSTD(double dailyStd);                   //need STD for S&P 500 & Russell 2000
+		double CalcFV(double portRet, double size, int years);
 		void CalcDollarAmts();			//using weights of preferred portfolio& initial investment size, calc $ amts for each stock
 		Eigen::RowVectorXd GetDollarAmts();        //  Frame needs to be able to get the dollar amts from outputstats so it can fill in the list
 		std::vector <std::string> GetTickers();
@@ -27,10 +28,11 @@ class outputstats {
 
 		Eigen::RowVectorXd weights;
 		Eigen::RowVectorXd values;
-		
+		Eigen::VectorXd stockBetas;
 		Eigen::RowVectorXd g2lweights;
 		Eigen::RowVectorXd g2lvalues;
+		Eigen::VectorXd product;
 
 		double invest;
-		
+		double portBeta;
 };
